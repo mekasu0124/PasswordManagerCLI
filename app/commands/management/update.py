2073283a95731dbc01491b87e403dce2c6b1c069
Mekasu <mekasurenae@gmail.com>
2024-12-31 15:44:24 -0600

    updated command to also check if new username already exists for the given link

--- app/commands/management/update.py
+++ app/commands/management/update.py
@@ -8,7 +8,7 @@ import click
 @click.option("--password", required=True, help="The new password currently saved.")
 @click.option("--new-username", required=False, help="The new username you want to set")
 @click.option("--new-password", required=False, help="The new password you want to set")
-def update_command(link: str, username: str, password: str, new_username: str, new_password: str):
+def update_entry(link: str, username: str, password: str, new_username: str, new_password: str):
     """
     Allows the user to update an existing entry for a given link and username.
     The link will remain unchanged, but the username and/or password can be updated.
@@ -26,14 +26,21 @@ def update_command(link: str, username: str, password: str, new_username: str, n
             break
 
     if entry_to_update:
-        if password:
-            entry_to_update["password"] = password
-
+        # Check if the new username already exists for the given link
         if new_username:
+            for entry in current_data:
+                if entry["link"] == link and entry["username"] == new_username:
+                    click.echo("That Username Already Exists")
+                    return
+
+            # If the username doesn't exist, update it
             entry_to_update["username"] = new_username
 
-        result = json_engine.update_entry(entry_to_update, current_data)
+        if new_password:
+            entry_to_update["password"] = new_password
 
+        # Save the updated entry
+        result = json_engine.update_entry(entry_to_update, current_data)
         click.echo(result)
     else:
         click.echo("Entry not found.")
