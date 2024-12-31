70fd1a16adfbcc6209f2cb7d563411863df24dfb
KastienDev <kastiendev@gmail.com>
2024-12-31 18:21:29 -0500

    full rich
    implemented rich fully throughout the application

--- app/commands/management/delete.py
+++ app/commands/management/delete.py
@@ -1,42 +1,36 @@
 from app.utilities.json import JsonEngine
-
 import click
+from rich.console import Console
+from rich.panel import Panel
 
+console = Console()
 
 @click.command(name="delete")
 @click.option("--link", required=True, help="The link of the entry to delete")
 @click.option("--username", required=True, help="The username of the entry to delete")
 def delete_entry(link: str, username: str):
-    """
-    Allows the user to delete an existing entry from the given link and username.
-    """
-
     json_engine = JsonEngine()
-
     all_entries = json_engine.list_all_entries()
-
     entry_to_delete = None
-
     for index, entry in enumerate(all_entries):
         if entry["link"] == link and entry["username"] == username:
             entry_to_delete = entry
             break
-        
     if entry_to_delete:
-        click.echo(f"Found Entry:\n\nLink: {entry_to_delete["link"]}\nUsername: {entry_to_delete["username"]}\nPassword: {entry_to_delete["password"]}")
-            
+        panel = Panel(
+            f"Found Entry:\n\nLink: {entry_to_delete['link']}\nUsername: {entry_to_delete['username']}\nPassword: {entry_to_delete['password']}",
+            title="[bold #1f2f45]Delete Entry[/bold #1f2f45]",
+            border_style="#849da2"
+        )
+        console.print(panel)
         user_agree = input("\nAre You Sure You Want To Delete This Entry? (Y/N): ")
-
         while not user_agree.lower() in ["y", "n"]:
-            click.echo("Invalid Input. Enter 'Y' for Yes or 'N' for No")
+            console.print("[bold #272123]Invalid Input. Enter 'Y' for Yes or 'N' for No[/bold #272123]")
             user_agree = input("Are You Sure You Want To Delete This Entry? (Y/N): ")
-
             if user_agree.lower() in ["y", "n"]:
                 break
-
         if user_agree.lower() == 'n':
-            return click.echo("You Elected To Keep This Entry.")
-        
+            console.print("[bold #385d8d]You Elected To Keep This Entry.[/bold #385d8d]")
+            return
         result = json_engine.delete_entry(entry_to_delete)
-
-        return click.echo(result)
+        console.print(f"[bold #385d8d]{result}[/bold #385d8d]")
--- app/utilities/json.py
+++ app/utilities/json.py
@@ -3,88 +3,57 @@ import os
 
 class JsonEngine:
     def check_exists(self):
-        # Get the directory of the current script (utilities/json.py)
         curr_dir = os.path.dirname(os.path.abspath(__file__))
-        # Navigate to the app directory
         app_dir = os.path.dirname(curr_dir)
-        # Define the .data directory path
         data_dir = os.path.join(app_dir, ".data")
-
-        # Create the .data directory if it doesn't exist
         if not os.path.isdir(data_dir):
             os.makedirs(data_dir, exist_ok=True)
-
-        # Define the path to the db.json file
         db_file = os.path.join(data_dir, "db.json")
-
-        # Create the db.json file if it doesn't exist
         if not os.path.isfile(db_file):
             with open(db_file, 'w+', encoding="utf-8-sig") as new:
                 json.dump([], new, indent=2)
-
         return db_file
 
     def list_all_entries(self):
         file_path = self.check_exists()
-
         with open(file_path, 'r', encoding="utf-8-sig") as f:
             data = json.load(f)
-
             return data if data else None
 
     def save_entry(self, new_entry: dict):
         file_path = self.check_exists()
-
         with open(file_path, 'r', encoding="utf-8-sig") as f:
             data = json.load(f)
-
             if not data:
                 data.append(new_entry)
-
                 with open(file_path, 'w+', encoding="utf-8-sig") as new:
                     json.dump(data, new, indent=2)
-
                 return "Entry Saved Successfully"
-
             for saved_entry in data:
-                link = saved_entry["link"]
-                username = saved_entry["username"]
-                password = saved_entry["password"]
-
-                if new_entry["link"] == link and new_entry["username"] == username:
-                    return f"\nEntry Already Exists!\n\nUsername: {username}\nLink: {link}\nPassword: {password}"
-
+                if new_entry["link"] == saved_entry["link"] and new_entry["username"] == saved_entry["username"]:
+                    return f"\nEntry Already Exists!\n\nUsername: {saved_entry['username']}\nLink: {saved_entry['link']}\nPassword: {saved_entry['password']}"
             data.append(new_entry)
-
             with open(file_path, 'w+', encoding="utf-8-sig") as new:
                 json.dump(data, new, indent=4)
-
             return "Entry Saved Successfully"
 
     def update_entry(self, updated_entry: dict, current_data: list):
         file_path = self.check_exists()
-
         for i, entry in enumerate(current_data):
             if entry["link"] == updated_entry["link"] and entry["username"] == updated_entry["username"]:
                 current_data[i] = updated_entry
                 break
-
         with open(file_path, 'w+', encoding="utf-8-sig") as new:
             json.dump(current_data, new, indent=2)
-
         return "Entry Updated Successfully"
 
     def delete_entry(self, entry_to_delete: dict):
         file_path = self.check_exists()
-
         with open(file_path, 'r', encoding="utf-8-sig") as f:
             data = json.load(f)
-
             for i, entry in enumerate(data):
                 if entry["link"] == entry_to_delete["link"] and entry["username"] == entry_to_delete["username"]:
                     data.pop(i)
-
                     with open(file_path, 'w+', encoding="utf-8-sig") as new:
                         json.dump(data, new, indent=2)
-
                     return "Entry Deleted Successfully"
