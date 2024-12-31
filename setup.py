56112ac92890469258f2c306df69c7bdb32865b1
Mekasu <mekasurenae@gmail.com>
2024-12-31 13:52:47 -0600

    updated setup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="password-manager",
-    version="1.0.0",
+    name='password_manager',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
-        "click"
+        'Click',  # Ensure Click is listed as a dependency
     ],
     entry_points={
-        "console_scripts": {
-            "pwm=app.main:main"
-        }
-    }
+        'console_scripts': [
+            'pwm=app.main:cli',  # Replace 'cli' with the name of your Click command function
+        ],
+    },
 )
