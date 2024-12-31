b277ef734256a3a71a93031d3032bac6528a76a6
Mekasu <mekasurenae@gmail.com>
2024-12-31 15:29:29 -0600

    updated test to check for fail case

--- tests/test_add_command.py
+++ tests/test_add_command.py
@@ -1,16 +1,15 @@
 from click.testing import CliRunner
-from app.commands.management.add import add_pw_command
+from app.commands.management.add import add_command
 
 def test_add_command():
     runner = CliRunner()
 
     # Correctly pass all arguments and options in a single list
     result = runner.invoke(
-        add_pw_command,
+        add_command,
         ["--link", "https://google.com", "--username", "mekasu0124", "--password", "Test123!"],
         input="y"
     )
 
     assert result.exit_code == 0
-    # Optionally, you can also check the output to ensure it's correct
-    assert "Data Saved" in result.output
+    assert "Entry Saved" in result.output or "\nEntry Already Exists" in result.output
